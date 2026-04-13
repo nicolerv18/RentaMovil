0c4089dfcf6f149735f8d8bdc6ffc909adaa12df
Thiago <thiagorojasg451@gmail.com>
2026-04-13 17:15:02 -0500

    Pago v 1.0 -- Seccion derecha completa

--- Front-end/rentaMovil-front-end/src/pages/Payment.jsx
+++ Front-end/rentaMovil-front-end/src/pages/Payment.jsx
@@ -3,11 +3,11 @@ import ButtonBack from "../components/ButtonBack";
 import ImgMapa from "../assets/MapaNeivaPago.png";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "./Payment.css";
 
 
 function Payment() {
   const navigate = useNavigate();
-  
   return (
   <>
     <Navbar />
@@ -21,9 +21,46 @@ function Payment() {
     <div className="container">
       <div className="left-section">
         <img src={ImgMapa} alt="Mapa" className="map-image" />
+        <div className="Details-ubi-1">
+          <p>12/07/26 10:00 AM</p>
+          <p>Descripcion de ubicacion</p>
+        </div>
+        <div className="Details-ubi-2">
+          <p>13/07/26 07:00 pm</p>
+          <p>Descripcion de ubicacion 2 </p>
+        </div>
       </div>
       <div className="right-section">
-        <h1>Payment</h1>
+        <div className="resumen-section">
+          <h2>Resumen</h2>
+          <div className="resumen-item">
+            <p>3 Dias X $403.000</p>
+            <p>Seguro todo riesgo</p>
+          </div>
+          <div className="total-section">
+            <h3>TOTAL</h3>
+            <p className="total-price">$ 403.000</p>
+          </div>
+        </div>
+
+        <div className="monto-section">
+          <h3>Monto a pagar</h3>
+          <p className="monto-desc">recuerde que debe ser mas del 30% del pago total</p>
+          <div className="monto-input">
+            <select defaultValue="30">
+              <option value="30">30%</option>
+              <option value="50">50%</option>
+              <option value="70">70%</option>
+              <option value="100">100%</option>
+            </select>
+          </div>
+        </div>
+        <div className="responsable-section">
+          <h3>Responsable Reserva</h3>
+          <input type="text" placeholder="Nombre" />
+          <input type="text" placeholder="Ciudad" />
+          <input type="text" placeholder="Direccion" />
+        </div>
       </div>
     </div>
     </div>
@@ -31,8 +68,4 @@ function Payment() {
   </>
   );
 }
-
-
-
-
 export default Payment;
