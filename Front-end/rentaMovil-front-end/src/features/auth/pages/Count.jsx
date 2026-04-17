11d47af020588588bf24258b2b595f02224bb908
nicolerv18 <nicolerv18007@gmail.com>
2026-04-17 16:04:56 -0500

    Feature: better style

--- Front-end/rentaMovil-front-end/src/features/auth/pages/Count.jsx
+++ Front-end/rentaMovil-front-end/src/features/auth/pages/Count.jsx
@@ -1,43 +1,88 @@
 import '../pages/Count.css';
 import login from '../../../assets/login.png';
-import {FaEdit, FaMoon} from "react-icons/fa";
+import { FaEdit, FaMoon, FaSun, FaGlobe } from "react-icons/fa";
 import Navbar from "../../../shared/components/layout/Navbar";
 import Footer from '../../../shared/components/layout/Footer';
+import { Link, useNavigate } from 'react-router-dom';
+import ButtonBack from '../../../shared/components/buttonBack';
 
-function Count() {
-return(
-    <>
-    <Navbar />
-    <div className='containerC'>
-    <div className='cardC'>
-    <div className="actions">
-    <button className="icon-btn"><FaEdit /></button>
-    <button className="icon-btn"><FaMoon /></button>
-</div>
-    <div className='formC'>
-    <div>
-    <img className="img" src={login}  alt='login'/>
-    </div>
-    <div>
-    <label className='form-labelC' htmlFor="nombre">Nombre:</label>
-    <input  type="text" placeholder='Nombre'/>
-    </div>
-    <div>
-    <label className='form-labelC' htmlFor="nombre">Telefono: </label>
-    <input type="text" placeholder='Nombre' />
-    </div>
-    <div>
-    <label className='form-labelC' htmlFor="nombre">Correo Electronico:</label>
-    <input className='email' type="email" placeholder='Correo Electronico' />
-    </div>
-    <button className='buttonC'>Modificar Contraseña</button>
-    </div>
-    </div>
-    </div>
-    <Footer />
-    </>
-    
-);
+function Count({ darkMode, setDarkMode }) {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Navbar />
+
+            <div className='containerC'>
+                <div className='cardC'>
+
+                    {/* HEADER */}
+                    <div className="header-page">
+                        <ButtonBack onClick={() => navigate(-1)} />
+                    </div>
+
+                    {/* ACCIONES */}
+                    <div className="actions">
+                        <button className="icon-btn">
+                            <FaEdit />
+                        </button>
+
+                        <button
+                            className="icon-btn"
+                            onClick={() => setDarkMode(!darkMode)}
+                        >
+                            {darkMode ? <FaSun /> : <FaMoon />}
+                        </button>
+
+                        <button className='icon-btn'>
+                            <FaGlobe />
+                        </button>
+                    </div>
+
+                    {/* FORM */}
+                    <div className='formC'>
+
+                        {/* IMAGEN */}
+                        <div className='form-image'>
+                            <img className="img" src={login} alt='login' />
+                        </div>
+
+                        {/* NOMBRE */}
+                        <div className='form-groupC'>
+                            <label className='form-labelC'>Nombre:</label>
+                            <input type="text" placeholder='Nombre' />
+                        </div>
+
+                        {/* TELÉFONO */}
+                        <div className='form-groupC'>
+                            <label className='form-labelC'>Teléfono:</label>
+                            <input type="text" placeholder='Teléfono' />
+                        </div>
+
+                        {/* EMAIL */}
+                        <div className='form-groupC'>
+                            <label className='form-labelC'>Correo Electrónico:</label>
+                            <input type="email" placeholder='Correo Electrónico' />
+                        </div>
+
+                        {/* PASSWORD */}
+                        <div className='form-groupC'>
+                            <label className='form-labelC'>Contraseña:</label>
+                            <input type="password" placeholder='Contraseña' />
+                        </div>
+
+                        {/* LINK */}
+                        <Link to='/ChangePassword' className='linkC'>
+                            Modificar Contraseña
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    );
 }
 
 export default Count;
