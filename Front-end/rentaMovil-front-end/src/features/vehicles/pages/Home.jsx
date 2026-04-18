cb558adbbe7e5ee29120907c7f401e2ee5fdf828
nicolerv18 <nicolerv18007@gmail.com>
2026-04-17 20:08:56 -0500

    Feature: new style menu

--- Front-end/rentaMovil-front-end/src/features/vehicles/pages/Home.jsx
+++ Front-end/rentaMovil-front-end/src/features/vehicles/pages/Home.jsx
@@ -4,38 +4,45 @@ import Footer from "../../../shared/components/layout/Footer.jsx";
 import  CartVehicule from "../components/CartVehicule"
 import Filtrer from "../components/Filtrer";
 import Banner  from "../../../shared/components/layout/Banner.jsx";
-import img from "../../../assets/carts/mustang.jpg";
+import img from "../../../assets/carts/car1.jpg";
 import img1 from "../../../assets/img/img1.png"
 import img2 from "../../../assets/img/img2.png"
 import img3 from "../../../assets/img/img3.webp"
+
 
 
 function Home(){
     return (
         <>
         <Navbar/>
-        <section className="home-container">
-          <div className="main-column">
+        <div className="banner-wrapper">
             <div className="banner-container">
-              <Banner imgs={[img1, img2, img3]} text="Disfruta la libertad de moverte cuando quieras con vehículos cómodos, seguros y en excelente estado. Ofrecemos alquiler rápido, precios accesibles y planes flexibles que se adaptan a tu viaje, ya sea por trabajo o placer. Elige tu vehículo ideal y conduce sin preocupaciones."/>
+                <Banner
+                  imgs={[img1, img2, img3]}
+                  text="Disfruta la libertad de moverte cuando quieras con vehículos cómodos, seguros y en excelente estado..."
+                />
             </div>
+        </div>
+        <section className="home-container">
+
+          <div className="main-column">
             <div className="card-vehicule-container">
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
-              <CartVehicule  name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
+              <CartVehicule name="MustangGT 500" age="2020" price="140.000" img={img} />
             </div>
           </div>
+
           <aside className="sidebar-container">
             <Filtrer/>
           </aside>
-        </section>
-        <Footer/>
 
+        </section>
+
+        <Footer/>
         </>
     )
 }
