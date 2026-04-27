0ce581b1accc6354cb472b17869de8e83278fb67
nicolerv18 <nicolerv18007@gmail.com>
2026-04-27 07:09:57 -0500

    feat: API

---
+++ model/src/main/java/com/rentamovil/repository/PermissionRepository.java
@@ -0,0 +1,16 @@
+package com.rentamovil.repository;
+
+import main.java.com.rentamovil.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
+
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+Optional<Permission> findByName(String name);
+List<Permission> findByDescription(String description);
+    boolean existsByName(String name);
+
+    List<Permission> findByRoles_RoleId(Long roleId);
+
+
+}
