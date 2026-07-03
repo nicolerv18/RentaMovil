44ca1d374240f85b2e4c93bf61730089d7205f22
Thiago <thiagorojasg451@gmail.com>
2026-07-03 12:51:37 -0500

    feat: initial pages

--- Front-end/rentaMovil-front-end/src/App.jsx
+++ Front-end/rentaMovil-front-end/src/App.jsx
@@ -11,6 +11,7 @@ import ChangePassword from './features/auth/pages/ChangePassword.jsx';
 import VehicleInventary from './features/admin/VehicleInventary/pages/VehicleInventary.jsx';
 // Vehicles
 import Home from './features/vehicles/pages/Home.jsx';
+import HomeS from './features/vehicles/pages/HomeS.jsx';
 
 // Booking
 import Reservation from './features/booking/pages/Reservation.jsx';
@@ -70,6 +71,7 @@ function App() {
         <Route path="/count" element={<Count theme={theme} setTheme={setTheme} />} />
         <Route path="/ChangePassword" element={<ChangePassword />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/homeS" element={<HomeS />} />
         <Route path="/reservation" element={<Reservation />} />
         <Route path="/HistorialReservation" element={<HistorialReservation />} />
         <Route path="/payment" element={<Payment />} />
---
+++ Front-end/rentaMovil-front-end/src/features/vehicles/components/CartVehiculeS.jsx
@@ -0,0 +1,43 @@
+import './CartVehicule.css'
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
+
+
+function CartVehicule({img,name,model,price, branch,type, door, capacity}){
+    const {t} = useTranslation();
+    const navigate = useNavigate();
+    return(
+<div className="card-vehicule">
+    <div className="car-img">
+        <img src={img} alt={name} />
+    </div>
+    {/* CENTRO - INFO */}
+    <div className="car-info">
+        <h3>{name}</h3>
+        <p className="car-type">{type}</p>
+
+        <div className="features">
+            <p><span className='text-box'>Modelo: </span>{model}</p>
+            <p><span className='text-box'>Capacidad: </span>{capacity}</p>
+            <span>🧳{door}</span>
+        </div>
+
+        <div className='location-container'>
+            <div className='location-box'>
+                <p className='location-text'>✈️</p> 
+            </div>
+            <div className='location-box'>
+                <p className='location-text'> {branch} </p> 
+            </div>
+        </div>
+    </div>
+    <div className="car-price">
+        <p className="price">${price}</p>
+        <span className="free">{t('cartVehicule.cancellation')}</span>
+        <button  onClick={() => navigate("/reservation", { state: { img, name, price, branch } })}>{t('cartVehicule.continue')}</button>
+    </div>
+
+</div>
+    )
+}
+export default CartVehicule;
