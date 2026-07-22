8f134c8f3be0b49687e8232f381825c329ff55a0
nicolerv18 <nicolerv18007@gmail.com>
2026-07-22 12:33:29 -0500

    Fix: fix folders

---
+++ app_mobile/rentaMovil/src/app/modal.tsx
@@ -0,0 +1,7 @@
+import Account from "../features/Account/pages/Account";
+
+export default function CountRoute() {
+  return <Account />;
+}
+
+
--- rentaMovil/src/app/modal.tsx
+++
@@ -1,7 +0,0 @@
-import Count from "../features/Count/pages/Count";
-
-export default function CountRoute() {
-  return <Count />;
-}
-
-
